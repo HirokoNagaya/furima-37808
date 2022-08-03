4b00df06f5023866e6b974e449935c632083b4b5
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-08-03 14:20:10 +0900

    テーブルに新規登録情報が保存されていることを確認した

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,22 @@ class User < ApplicationRecord
   validates :first_name, presence: true 
   validates :kana_last_name, presence: true
   validates :kana_first_name, presence: true
-  validates :date_of_birth, presence: true       
-         
+  validates :date_of_birth, presence: true
+  
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
+    
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :first_name
+      validates :last_name
+    end
+      
+    with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナ文字を使用してください' } do
+      validates :kana_last_name
+      validates :kana_first_name
+    end   
 end
+
+
+
+
