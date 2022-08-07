c71fc8485a57b92110c84407cd399574a5d88a7b
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-08-07 13:37:29 +0900

    ActiveHashの導入、　バリデーション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def  item_params
+    params.require(:item).permit(:item_name, :image, :description, :category_id, :condition_id, :shipping_charge_id, :sender_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,27 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :sender
+  belongs_to :day_to_ship
+  end
+
   belongs_to :user
+
+  has_one_attached :image
+
+  #メモ；空の投稿を保存できないようにする
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
+
+  #メモ；ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_charge_id, numericality: { other_than: 1 }
+  validates :sender_id, numericality: { other_than: 1 }
+  validates :day_to_ship_id, numericality: { other_than: 1 }     
 end
