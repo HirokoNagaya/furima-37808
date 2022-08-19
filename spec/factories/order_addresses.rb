4d14c0cccdf39d6c8610bb16fb9ba7505ea4cda0
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-08-19 11:04:26 +0900

    テストコード記述

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    postcode { '123-4567' }
+    sender_id { 1 }
+    city { '東京都' }
+    street { '1-1' }
+    building { '東京ハイツ' }
+    phone_number{ 19012345678 }
+  end
+end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '購入商品情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+      it 'buildingは空でも保存できること' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postcodeが空だと保存できないこと' do
+        @order_address.postcode = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postcode can't be blank")
+      end
+      it 'postcodeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @order_address.postcode = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Postcode is invalid. Include hyphen(-)')
+      end
+      it 'sender_idを選択していないと保存できないこと' do
+        @order_address.sender_id = 0
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Sender id can't be blank")
+      end
+      it 'cityが空だと保存できないこと' do
+        @order_address.city = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'streetが空だと保存できないこと' do
+        @order_address.street = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Street can't be blank")
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが半角のハイフンを含まない正しい形式でないと保存できないこと' do
+        @order_address.phone_number = '090-1234-5678'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid. Exclude hyphen(-)')
+      end
+    end
+  end
+end
