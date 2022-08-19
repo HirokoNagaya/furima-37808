22763ddbaea5c6abeafe763ba1f08aa5e645c167
pikopikotaro <ringo-hunter-dreamer.44.121@docomo.ne.jp>
2022-08-19 09:05:23 +0900

    フォーム内容の保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,26 +2,28 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @order_addresses = OrderAddress.all
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
-  def new
-    @order_address = OrderAddress.new
-  end
+  
 
   def create
-    @dorder_address = OrderAddress.new(order_params)
+   
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:postcode, :sender_id, :city, :street, :building, :phone_number, :order, :item).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:postcode, :sender_id, :city, :street, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,15 +10,14 @@ class OrderAddress
     validates :city
     validates :street
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Exclude hyphen(-)"}
-    validates :order_id
   end
-  validates :building
+  
 
   def save
-    # 寄付情報を保存し、変数donationに代入する
+    # 購入情報を保存し、変数order代入する
     order = Order.create(item_id: item_id, user_id: user_id)
     # 住所を保存する
-    # donation_idには、変数donationのidと指定する
+    # order_idには、変数orderのidと指定する
     Address.create(postcode: postcode, sender_id: sender_id, city: city, street: street, phone_number: phone_number, order_id: order.id)
   end
 end
